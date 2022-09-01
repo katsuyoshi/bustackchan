f5b925a9a423d1e2e686c18c3940fbadacdf189b
Katsuyoshi Ito <kito@itosoft.com>
2022-09-01 19:46:56 +0900

    bus face追加

---
+++ src/faces/bus_face.h
@@ -0,0 +1,220 @@
+#ifndef BUS_FACE
+#define BUS_FACE
+
+
+#include <Face.h>
+#include <Avatar.h>
+#include "BoundingRect.h"
+#include "DrawContext.h"
+#include "Drawable.h"
+#include "lgfx/Fonts/efont/lgfx_efont_ja.h"
+
+using namespace m5avatar;
+
+class BusMouth : public Drawable {
+ private:
+  uint16_t minWidth;
+  uint16_t maxWidth;
+  uint16_t minHeight;
+  uint16_t maxHeight;
+
+ public:
+  BusMouth() : BusMouth(90, 90, 20, 20) {}
+  BusMouth(uint16_t minWidth, uint16_t maxWidth, uint16_t minHeight,
+           uint16_t maxHeight)
+      : minWidth{minWidth},
+        maxWidth{maxWidth},
+        minHeight{minHeight},
+        maxHeight{maxHeight} {}
+
+  void draw(M5Canvas *spi, BoundingRect rect, DrawContext *ctx) {
+    //uint32_t primaryColor = ctx->getColorDepth() == 1 ? 1 : ctx->getColorPalette()->get(COLOR_PRIMARY);
+    //uint32_t secondaryColor = ctx->getColorDepth() == 1 ? 1 : ctx->getColorPalette()->get(COLOR_SECONDARY);
+    uint32_t primaryColor = ctx->getColorPalette()->get(COLOR_PRIMARY);
+    uint32_t secondaryColor = ctx->getColorPalette()->get(COLOR_SECONDARY);
+    float breath = _min(1.0f, ctx->getBreath());
+    float openRatio = ctx->getMouthOpenRatio();
+    int h = minHeight + (maxHeight - minHeight) * openRatio;
+    int w = minWidth + (maxWidth - minWidth) * (1 - openRatio);
+    int x = rect.getLeft() - w / 2;
+    int y = rect.getTop() - h / 2 + breath * 2;
+    spi->fillRect(x, y, w, h, primaryColor);
+    spi->setCursor(x + 4, y + 4);                        // 座標を指定（x, y）
+    spi->setTextSize(2.0);            // 文字倍率変更
+    spi->setTextColor(secondaryColor, primaryColor);
+    spi->println("BUSTACK");                // 表示内容をcanvasに準備
+
+    spi->fillRect(10, 10, 300, 100, primaryColor);
+    spi->setCursor(15, 15);                        // 座標を指定（x, y）
+    spi->setFont(&fonts::lgfxJapanGothic_12);
+    spi->setTextDatum( textdatum_t::top_center      );
+    spi->setTextSize(2.0);            // 文字倍率変更
+    spi->setTextColor(secondaryColor, primaryColor);
+    spi->println("スタック沼");                // 表示内容をcanvasに準備
+  }
+};
+
+class BusEye : public Drawable {
+ private:
+  uint16_t width;
+  uint16_t height;
+  uint16_t r;
+  bool isLeft;
+
+ public:
+  // constructor
+  BusEye(uint16_t width, uint16_t height, uint16_t r, bool isLeft)
+        : width{width},
+          height{height},
+          r{r},
+          isLeft{isLeft} {}
+  //~Eye() = default;
+  //Eye(const Eye &other) = default;
+  //Eye &operator=(const Eye &other) = default;
+
+  void draw(M5Canvas *spi, BoundingRect rect,
+            DrawContext *ctx) {
+    uint32_t primaryColor = ctx->getColorPalette()->get(COLOR_PRIMARY);
+    rect.setSize(width, height);
+    int x = rect.getLeft();
+    int y = rect.getTop();
+    int w = rect.getWidth();
+    int h = rect.getHeight();
+    //spi->fillRect(x, y, w, h, primaryColor);
+    spi->drawRect(x, y, w, h, primaryColor);
+
+    {
+      Expression exp = ctx->getExpression();
+      uint32_t x = rect.getCenterX();
+      uint32_t y = rect.getCenterY();
+      Gaze g = ctx->getGaze();
+      float openRatio = ctx->getEyeOpenRatio();
+      uint32_t offsetX = g.getHorizontal() * 3 + (isLeft ? - 8 : 8);
+      uint32_t offsetY = g.getVertical() * 3;
+      uint32_t primaryColor = ctx->getColorPalette()->get(COLOR_PRIMARY);
+      uint32_t backgroundColor = ctx->getColorDepth() == 1 ? 0 : ctx->getColorPalette()->get(COLOR_BACKGROUND);
+
+      if (openRatio > 0) {
+        spi->fillCircle(x + offsetX, y + offsetY, r, primaryColor);
+        // TODO(meganetaaan): Refactor
+        if (exp == Expression::Angry || exp == Expression::Sad) {
+          int x0, y0, x1, y1, x2, y2;
+          x0 = x + offsetX - r;
+          y0 = y + offsetY - r;
+          x1 = x0 + r * 2;
+          y1 = y0;
+          x2 = !isLeft != !(exp == Expression::Sad) ? x0 : x1;
+          y2 = y0 + r;
+          spi->fillTriangle(x0, y0, x1, y1, x2, y2, backgroundColor);
+        }
+        if (exp == Expression::Happy || exp == Expression::Sleepy) {
+          int x0, y0, w, h;
+          x0 = x + offsetX - r;
+          y0 = y + offsetY - r;
+          w = r * 2 + 4;
+          h = r + 2;
+          if (exp == Expression::Happy) {
+            y0 += r;
+            spi->fillCircle(x + offsetX, y + offsetY, r / 1.5, backgroundColor);
+          }
+          spi->fillRect(x0, y0, w, h, backgroundColor);
+        }
+      } else {
+        int x1 = x - r + offsetX;
+        int y1 = y - 2 + offsetY;
+        int w = r * 2;
+        int h = 4;
+        spi->fillRect(x1, y1, w, h, primaryColor);
+      }
+    }
+
+  }
+};
+
+
+class BusFace : public Face {
+ public:
+  BusFace()
+      : Face(
+          new BusMouth(90, 90, 20, 20), new BoundingRect(220, 163),
+           new BusEye(100, 60, 24, false), new BoundingRect(170, 1),
+           new BusEye(100, 60, 24, true), new BoundingRect(170, 220),
+           new Eyeblow(32, 0, false), new BoundingRect(67, 96),
+           new Eyeblow(32, 0, true), new BoundingRect(72, 230)) {}
+  
+#if 0
+  void draw(DrawContext *ctx) {
+    sprite->setColorDepth(ctx->getColorDepth());
+    // NOTE: setting below for 1-bit color depth
+    sprite->setBitmapColor(ctx->getColorPalette()->get(COLOR_PRIMARY),
+      ctx->getColorPalette()->get(COLOR_BACKGROUND));
+    sprite->createSprite(boundingRect->getWidth(), boundingRect->getHeight());
+    if (ctx->getColorDepth() != 1) {
+      sprite->fillSprite(ctx->getColorPalette()->get(COLOR_BACKGROUND));
+    }
+    float breath = _min(1.0f, ctx->getBreath());
+
+    // TODO(meganetaaan): unify drawing process of each parts
+    BoundingRect rect = *mouthPos;
+    rect.setPosition(rect.getTop() + breath * 3, rect.getLeft());
+    // copy context to each draw function
+    mouth->draw(sprite, rect, ctx);
+
+    rect = *eyeRPos;
+    rect.setPosition(rect.getTop() + breath * 3, rect.getLeft());
+    eyeR->draw(sprite, rect, ctx);
+
+    rect = *eyeLPos;
+    rect.setPosition(rect.getTop() + breath * 3, rect.getLeft());
+    eyeL->draw(sprite, rect, ctx);
+
+    rect = *eyeblowRPos;
+    rect.setPosition(rect.getTop() + breath * 3, rect.getLeft());
+    eyeblowR->draw(sprite, rect, ctx);
+
+    rect = *eyeblowLPos;
+    rect.setPosition(rect.getTop() + breath * 3, rect.getLeft());
+    eyeblowL->draw(sprite, rect, ctx);
+
+    // TODO(meganetaaan): make balloons and effects selectable
+    /*
+    b.draw(sprite, br, ctx);
+    h.draw(sprite, br, ctx);
+    */
+    // drawAccessory(sprite, position, ctx);
+
+
+    sprite->fillRect(10, 60, 300, 100, ctx->getColorPalette()->get(COLOR_SECONDARY));
+
+
+    // TODO(meganetaaan): rethink responsibility for transform function
+    float scale = ctx->getScale();
+    float rotation = ctx->getRotation();
+
+    if (scale != 1.0 || rotation != 0) {
+      uint16_t w = boundingRect->getWidth();
+      uint16_t h = boundingRect->getHeight();
+      uint16_t tmpSprWidth = _min(w * scale, 320);
+      uint16_t tmpSprHeight = _min(h * scale, 240);
+      tmpSpr->setColorDepth(ctx->getColorDepth());
+      tmpSpr->setBitmapColor(ctx->getColorPalette()->get(COLOR_PRIMARY),
+        ctx->getColorPalette()->get(COLOR_BACKGROUND));
+      tmpSpr->createSprite(tmpSprWidth, tmpSprHeight);
+      if (ctx->getColorDepth() != 1) {
+        sprite->fillSprite(ctx->getColorPalette()->get(COLOR_BACKGROUND));
+      }
+      transformSprite(sprite, tmpSpr, rotation, scale);
+
+      tmpSpr->pushSprite(boundingRect->getLeft(), boundingRect->getTop());
+      tmpSpr->deleteSprite();
+    } else {
+      sprite->pushSprite(boundingRect->getLeft(), boundingRect->getTop());
+    }
+
+    sprite->deleteSprite();
+  }
+#endif
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,59 @@
 #include <M5Unified.h>
 #include <Avatar.h>
+#include "ServoEasing.hpp"
+#include "faces/bus_face.h"
 
 using namespace m5avatar;
 
-M5GFX display;
+#define SERVO_PIN           5
+
+#define SERVO_START_DEGREE  90
 
 Avatar avatar;
+ServoEasing servo;
+ColorPalette cp;
+
+double pi = atan(1.0) * 4.0;
 
 void setup()
 {
   M5.begin();
-  avatar.init(); // start drawing
-  avatar.setRotation(PI);
+  //avatar.setPosition(50, 50);
+  //avatar.setRotation(PI * 180.00003 / 180.0);
+  avatar.setRotation(pi);
+  avatar.setFace(new BusFace());
+  cp.set(COLOR_PRIMARY, TFT_WHITE);
+  cp.set(COLOR_BACKGROUND, TFT_RED);
+  cp.set(COLOR_SECONDARY, TFT_BLACK);
+  avatar.setColorPalette(cp);
+  avatar.init(8); // start drawing
+
+  if (servo.attach(SERVO_PIN, SERVO_START_DEGREE)) {
+    Serial.print("Error attaching servo x");
+  }
+  delay(500);
 }
+
+bool moving = false;
+bool pressed = false;
 
 void loop()
 {
-  // avatar's face updates in another thread
-  // so no need to loop-by-loop rendering
+  M5.update();
+  if (M5.BtnB.wasPressed()) {
+    moving = !moving;
+  }
+
+  int f = 15;//65;
+  int r = 15;//55;
+  if (moving) {
+    servo.easeTo(SERVO_START_DEGREE + f, 40); // Blocking call
+    delay(1000);
+    servo.easeTo(SERVO_START_DEGREE - r, 40); // Blocking call
+    delay(1000);
+  } else {
+    Serial.println(F("Move to 0 degree  with 40 degree per second blocking"));
+    servo.easeTo(SERVO_START_DEGREE, 40); // Blocking call
+    delay(1000);
+  }
 }
